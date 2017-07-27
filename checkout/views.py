74a0d7bf241118129fa12f3f4ace2cafb1c76f2b
Ngahu <jamaalaraheem@gmail.com>
2017-07-27 04:42:43 +0300

    making of the checkout urls

--- checkout/views.py
+++ checkout/views.py
@@ -2,7 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+
+@login_required
+def checkout(request):
+    context = {
+
+    }
+    return render(request,"checkout.html",context)
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('booking.urls',namespace='booking')),
+    #url(r'^checkout/',include('checkout.urls',namespace='checkout')),
     url(r'^accounts/', include('allauth.urls')),
     ]
 
